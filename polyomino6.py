59d3977f51afcc6a28fc3c6bacdef5ac2ea8c2b1
Eluee <touch1027@naver.com>
2023-04-05 16:41:18 +0900

    2023-04-05

--- polyomino6.py
+++ polyomino6.py
@@ -1,30 +1,27 @@
 maps = []
-
 max_set = set()
-
 resort = 0
-
-
 
 def compare( set ):
     global resort, map_size, maps
     total = 0
     for index in tuple(set):
-        print(index[0] , " " , index[1])
-        #total += map[index[0]][index[1]]
+        #print(index[0] , " &" , index[1])
+        total += maps[index[0]][index[1]]
 
     if total >= resort: resort = total
 
 
 def find(x, y, duplicate, set):
     global max_set, map_size
-    if(0 > x or x > map_size[1] or 0 > y or  y > map_size[0]): return
+    if(0 > x or x >= map_size[1] or 0 > y or  y >= map_size[0]): return
     cp_set = set.copy()
     if (y, x) in cp_set:
         duplicate += 1
         if duplicate >= 2:
             return
     elif len(cp_set) >= 4:
+        #print(cp_set)
         compare(cp_set)
         return
     else:
@@ -35,7 +32,6 @@ def find(x, y, duplicate, set):
     find(x, y + 1, duplicate, cp_set) 
     find(x, y - 1, duplicate, cp_set)     
     
-
 map_size = list(map(int, input().split()))
 
 for i in range(map_size[0]):
