5aec3b83a0656e0b6b7fe2546b70aefdf316a2d6
Eluee <touch1027@naver.com>
2023-05-25 11:49:20 +0900

    2023-05-25-2

--- RPA/RPA_2.py
+++ RPA/RPA_2.py
@@ -1,12 +1,16 @@
 from openpyxl import Workbook
+from openpyxl import load_workbook # 워크북 
 from random import *
 
-wb = Workbook()
+# load_workbook() 함수는 파일을 불러올 수 있다.
+#
+wb = load_workbook("testsampleb.xlsx")
 ws = wb.active
 
-ws.append(["번호", "영어", "수학"])
-for i in range(1, 11):
-    ws.append([i, randint(0, 100), randint(0, 100)])
+# iter_rows()함수의 매개변수 값이 min_row = 2 면 2번 행을 포함한다. 즉 min_row = 1만 있다면 결국 한줄을 포함하기 때문에 전체 행을 말한다.
 
-wb.save("testsampleb.xlsx")
+for row in ws.iter_rows(min_row = 2):
+    if int(row[1].value) > 50:
+        print(row[0].value, "번 학생은 50점을 넘었습니다.")
+
 wb.close()
