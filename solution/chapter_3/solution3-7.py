616a37a64644ed3104b9b7fcfb8ef8948bc3c7c8
Eluee <touch1027@naver.com>
2023-06-17 12:24:33 +0900

    2023-06-17 code fix

--- solution/chapter_3/solution3-7.py
+++ solution/chapter_3/solution3-7.py
@@ -3,7 +3,7 @@ def solution(num_apple, num_carrot, k):
     max_juice = num_apple // 3 if num_apple // 3 < num_carrot else num_carrot
     #주스를 최대로 만들고 남은 과일 수
     remain_fruit = (num_apple - (max_juice * 3)) + (num_carrot - max_juice)  
-    if remain_fruit < k and 0 < remain_fruit :
+    if remain_fruit < k :
       # 먹이의 갯수가 남는 과일 수보다 더 많을 때 
       k = k - remain_fruit
       max_juice -= k // 4
