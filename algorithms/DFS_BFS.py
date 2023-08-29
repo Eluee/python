06642b6b2ef22f66a50710c3dec233184d5e4ab5
Eluee <touch1027@naver.com>
2023-08-29 10:34:18 +0900

    graph_data

---
+++ algorithms/DFS_BFS.py
@@ -0,0 +1,17 @@
+
+## 예제 graph
+def create_graph():
+    graph = dict()
+    graph['A'] = ['B', 'C']
+    graph['B'] = ['A', 'D']
+    graph['C'] = ['A', 'G', 'H', 'I']
+    graph['D'] = ['B', 'E', 'F']
+    graph['E'] = ['D']
+    graph['F'] = ['D']
+    graph['G'] = ['C']
+    graph['H'] = ['C']
+    graph['I'] = ['C', 'J']
+    graph['J'] = ['I']
+    return graph
+
+print(create_graph())
