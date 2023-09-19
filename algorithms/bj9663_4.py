c387f310dfe8906a5540c3325fb0e0c417845edd
Eluee <touch1027@naver.com>
2023-09-19 11:48:51 +0900

    2023-09-19

---
+++ algorithms/bj9663_4.py
@@ -0,0 +1,68 @@
+
+import math
+import time
+
+
+def locate_queen(point, map):
+    """ 
+    퀸이 point 좌표에 있을때의 공격 가능한 좌표를 제거
+    Args:
+        point (_tuple_): _퀸이 위치한 x, y 좌표를 담고 있는 tuple
+        map (_set_): _지도에 존재하는 좌표_
+    """
+    # 직선 경로
+    global N
+    i, j = point
+    for x in range(N): 
+        if (x, j) in map: map.remove((x, j))
+        if (i, x) in map: map.remove((i, x))
+        
+    # 대각선 경로
+    d = i - j
+    if d <= 0:
+        for x in range(N + d): 
+            if (x, x - d) in map: map.remove((x, x - d))
+    else:
+        for x in range(0 + d, N):
+            if (x, x - d) in map: map.remove((x, x - d))
+    # 반대 대각선 경로
+    temp =  j + i
+    if temp < N:
+        for x in range(temp + 1):
+            if (x, temp - x) in map: map.remove((x, temp - x))
+    else:
+        for x in range(temp + 1 - N, N ):
+            if (x, temp - x) in map: map.remove((x, temp - x))
+
+def solution(queens ,map):
+    global N, result
+    
+    if len(queens) == N:
+        result.append(queens)
+        view_table(queens)
+        return
+  
+    for index in range(len(map)):
+        queens_cp = queens.copy()
+        map_cp = map[index + 1:]
+        queens_cp.append(map[index])
+        locate_queen(map[index], map_cp )
+        solution(queens_cp, map_cp)
+
+def view_table(queens):
+    table = [['0' for _ in range(N)] for _ in range(N)]
+    for coord in queens:
+        table[coord[0]][coord[1]] = '#'
+    
+    print(queens)
+    for row in table:
+        print(* row)
+    print()
+
+N = int(input())
+map = [(i, j) for i in range(N) for j in range(N)]
+result = []
+
+
+solution([],map)
+print(len(result))
