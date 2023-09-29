0e7ab307113380eb59256209dea0b1db4f8e6c8d
Eluee <touch1027@naver.com>
2023-09-29 20:00:17 +0900

    bj1654

--- algorithms/bj1654_2.py
+++ algorithms/bj1654_2.py
@@ -3,5 +3,21 @@ cable_list = [int(input()) for _ in range(K)]
     
 
 
-max_len = min(cable_list)
+def valid_cable(cut_length):
+    global cable_list
+    return sum([cable // cut_length for cable in cable_list])
+
+left = 1
+# 안자르는 케이블이 존재할 수 있기 때문에 가장 큰 값으로 시작
+right = max(cable_list)
+
+while left < right :
+    mid = (left + right) // 2 + 1
+    cut_cables = valid_cable(mid)
+    if N <= cut_cables:
+        left = mid
+    else:
+        right = mid - 1
+
+print(left)
 
