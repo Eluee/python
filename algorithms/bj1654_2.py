39c2dc67dcd9f6577a8299e788de8d876a50a079
Eluee <touch1027@naver.com>
2023-10-11 21:10:20 +0900

    crrent

--- algorithms/bj1654_2.py
+++ algorithms/bj1654_2.py
@@ -8,7 +8,7 @@ def valid_cable(cut_length):
     return sum([cable // cut_length for cable in cable_list])
 
 left = 1
-# 안자르는 케이블이 존재할 수 있기 때문에 가장 큰 값으로 시작
+# 안자르는 케이블이 존재할 수 있기 때문에 가장 큰 값으로 시작 전에는 모든 캐이블을 잘라야한다는 생각에 가장 작은 값을 사용해서 오답이남..
 right = max(cable_list)
 
 while left < right :
