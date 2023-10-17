0fa5ba7fe29e64d5545e0a98a387c959dfc263a0
Eluee <touch1027@naver.com>
2023-10-17 21:25:43 +0900

    bj1477

--- algorithms/bj1477.py
+++ algorithms/bj1477.py
@@ -1,3 +1,30 @@
-N, M, L = list(map(int, input().split()))
-rest_stop_list = sorted(list(map(int, input().split())))
+from operator import truediv
+import sys
 
+N, M, L = list(map(int, sys.stdin.readline().split()))
+reststop_list = sorted(list(map(int, sys.stdin.readline().split())))
+reststop_list = [0] + reststop_list + [L]
+
+dist_list = sorted([reststop_list[i+1] - reststop_list[i] for i in range(len(reststop_list) - 1)],reverse=True)
+
+for i in range(M):
+    if dist_list[i] % 2 == 0:
+        dist = [dist_list[i] // 2] * 2
+    else:
+        dist = [dist_list[i] // 2, dist_list[i] // 2 + 1]
+        
+    left = i + 1
+    right = len(dist_list) - 1
+    while(left < right):
+        mid = (left + right) // 2
+        if  dist_list[mid] < dist_list[0] :
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    print(left)
+            
+            
+            
+# 찾으려고 하는 숫자는 dist_list[i] 보다 큰 숫자가 시작하는 곳
+        
